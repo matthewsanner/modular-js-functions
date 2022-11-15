0d92a30a75e09e233e72e797cd84395c09fbaf74
Matthew Sanner <gravityskunk@gmail.com>
2022-11-15 11:21:29 -0600

    added sort functions

--- index.js
+++ index.js
@@ -4,5 +4,11 @@ function sumArray(ar) {
 }
 
 //order ascending numerically
+function sortAsc(ar) {
+    ar.sort((a, b) => a - b);
+}
 
 //order descending numerically
+function sortDes(ar) {
+    ar.sort((a, b) => b - a);
+}
