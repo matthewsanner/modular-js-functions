b68c87251e87991147948b398421404aa20370ea
Matthew Sanner <gravityskunk@gmail.com>
2022-11-16 11:52:42 -0600

    fixed functions

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ function sumArray(ar) {
 
 //order ascending numerically
 function sortAsc(ar) {
-    ar.sort((a, b) => a - b);
+    return ar.sort((a, b) => a - b);
 }
 
 //order descending numerically
 function sortDes(ar) {
-    ar.sort((a, b) => b - a);
+    return ar.sort((a, b) => b - a);
 }
